f2378ee081d342545b45565d12c2e30b2c2bc9ff
noah <dtd12321@gmail.com>
2023-05-03 16:05:24 +0900

    create xcode project

---
+++ podong4cuts/podong4cuts/podong4cutsApp.swift
@@ -0,0 +1,17 @@
+//
+//  podong4cutsApp.swift
+//  podong4cuts
+//
+//  Created by 이승용 on 2023/05/03.
+//
+
+import SwiftUI
+
+@main
+struct podong4cutsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
