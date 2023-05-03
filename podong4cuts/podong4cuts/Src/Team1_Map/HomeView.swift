4629aec76cd060c3152eef47e4def1701303ac43
mininim <minsoplee2015@gmail.com>
2023-05-03 16:45:57 +0900

    초기 프로젝트 설정
    파일별로 작업하자!

---
+++ podong4cuts/podong4cuts/Src/Team1_Map/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  podong4cuts
+//
+//  Created by Eric Lee on 2023/05/03.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
---
+++ podong4cuts/podong4cuts/Src/Team3_FCS/FourCutStudioView.swift
@@ -0,0 +1,20 @@
+//
+//  FourCutStudioView.swift
+//  podong4cuts
+//
+//  Created by Eric Lee on 2023/05/03.
+//
+
+import SwiftUI
+
+struct FourCutStudioView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FourCutStudioView_Previews: PreviewProvider {
+    static var previews: some View {
+        FourCutStudioView()
+    }
+}
