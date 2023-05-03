567905426aa0e49fc933c67bfe8824a4a1ad3edf
BAE <lexon1209@gmail.com>
2023-05-04 01:55:37 +0900

    remove .DS_Store and add test-sean.swift

---
+++ podong4cuts/podong4cuts/Src/Team1_Map/test_sean.swift
@@ -0,0 +1,20 @@
+//
+//  test_sean.swift
+//  podong4cuts
+//
+//  Created by BAE on 2023/05/04.
+//
+
+import SwiftUI
+
+struct test_sean: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct test_sean_Previews: PreviewProvider {
+    static var previews: some View {
+        test_sean()
+    }
+}
