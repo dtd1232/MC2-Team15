fb7e032fa389422a085be513b89ca5dc2a3449cf
BAE <lexon1209@gmail.com>
2023-05-08 16:00:12 +0900

    Feat: 션 + 노아 코드 머지
    TODO: 맵 뷰 애니메이션 적용

--- podong4cuts/podong4cuts/Src/Team1_Map/MapView.swift
+++ podong4cuts/podong4cuts/Src/Team1_Map/MapView.swift
@@ -10,27 +10,40 @@ import MapKit
 import CoreLocation
 
 struct MapView: View {
-//    @State private var region = MKCoordinateRegion(
-//        center: CLLocationCoordinate2D(latitude: 36.058616, longitude: 129.382959),
-//        span: MKCoordinateSpan(latitudeDelta: 0.015,
-//                               longitudeDelta: 0.015))
-//    @State var offset: CGFloat = 0
-//    @State var tracking: MapUserTrackingMode = .follow
-//    @State var flags: Bool = false
-//    @State private var reloadMapView = false
+    //    @State private var region = MKCoordinateRegion(
+    //        center: CLLocationCoordinate2D(latitude: 36.058616, longitude: 129.382959),
+    //        span: MKCoordinateSpan(latitudeDelta: 0.015,
+    //                               longitudeDelta: 0.015))
+    //    @State var offset: CGFloat = 0
+    @State var tracking: MapUserTrackingMode = .follow
+    //    @State var flags: Bool = false
+    //    @State private var reloadMapView = false
     @StateObject var manager = LocationManager()
     @State private var currentLocationManager = CLLocationManager()
+    
+    let MapLocations = [
+        MapLocation(name: "영일교",
+                    latitude: 36.06149,
+                    longitude: 129.38306
+                   ),
+        MapLocation(name: "Space Walk",
+                    latitude: 36.06437,
+                    longitude: 129.38937
+                   )
+    ]
     
     var body: some View {
         VStack{
             ZStack{
-                MapViewModel(region: $manager.region, showUserLocation: true)
-//                    .id(reloadMapView)
-                //                Map(coordinateRegion: $manager.region,
-                //                    interactionModes: MapInteractionModes.all,
-                //                    showsUserLocation: true,
-                //                    userTrackingMode: $tracking)
-                //                .id(reloadMapView)
+                //                MapViewModel(region: $manager.region, showUserLocation: true)
+                //                    .id(reloadMapView)
+                Map(coordinateRegion: $manager.region,
+                    interactionModes: MapInteractionModes.all,
+                    showsUserLocation: true,
+                    userTrackingMode: $tracking,
+                    annotationItems: MapLocations) { locations in
+                    MapMarker(coordinate: locations.coordinate, tint: .red)
+                }
                 
                 HStack {
                     VStack(alignment: .leading) {
@@ -81,22 +94,26 @@ struct MapView: View {
         guard let userLocation = currentLocationManager.location?.coordinate else { return }
         manager.region = MKCoordinateRegion(center: userLocation, span: MKCoordinateSpan(latitudeDelta: 0.015, longitudeDelta: 0.015))
     }
+    
+    func updateUIView(_ uiView: MKMapView) {
+        uiView.setRegion(manager.region, animated: true)
+    }
 }
 
-struct MapViewModel: UIViewRepresentable {
-    @Binding var region: MKCoordinateRegion
-    var showUserLocation: Bool
-    
-    func makeUIView(context: Context) -> MKMapView {
-        let mapView = MKMapView()
-        mapView.showsUserLocation = showUserLocation
-        return mapView
-    }
-    
-    func updateUIView(_ uiView: MKMapView, context: Context) {
-        uiView.setRegion(region, animated: true)
-    }
-}
+//struct MapViewModel: UIViewRepresentable {
+//    @Binding var region: MKCoordinateRegion
+//    var showUserLocation: Bool
+//
+//    func makeUIView(context: Context) -> MKMapView {
+//        let mapView = MKMapView()
+//        mapView.showsUserLocation = showUserLocation
+//        return mapView
+//    }
+//
+//    func updateUIView(_ uiView: MKMapView, context: Context) {
+//        uiView.setRegion(region, animated: true)
+//    }
+//}
 
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
