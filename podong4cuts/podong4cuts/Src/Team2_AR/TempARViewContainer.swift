12f5b4cab22d96614e2557e54af5c8f447caf545
Lets <ruvan123@naver.com>
2023-05-10 01:02:31 +0900

    feat: make TempARViewContainer for test ( delete later )

---
+++ podong4cuts/podong4cuts/Src/Team2_AR/TempARViewContainer.swift
@@ -0,0 +1,61 @@
+//
+//  TempARViewContainer.swift
+//  podong4cuts
+//
+//  Created by user on 2023/05/10.
+//
+
+import Foundation
+
+import Foundation
+import SwiftUI
+import RealityKit
+import ARKit
+import CoreImage
+
+struct TempARViewContainer: UIViewRepresentable {
+    var arView: ARView
+    
+    init() {
+        arView = ARView(frame: .zero)
+    }
+    
+    func makeUIView(context: Context) -> ARView {
+        
+//        arView = ARView(frame: .zero)
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+        
+        return arView
+    }
+    
+    func updateUIView(_ arView: ARView, context: Context) {}
+    
+    
+    
+    func takeSnapShot(center: CGPoint, width: CGFloat, height: CGFloat) {
+        DispatchQueue.global(qos: .userInitiated).async {
+            arView.snapshot(saveToHDR: false) { (image) in
+//                print(image!.size.width)
+//                print(image!.size.height)
+//                print(width, height)
+                let compressedImage = UIImage(data: (image?.pngData())!)
+                let croppedImage = compressedImage?.crop(rect: CGRect(x: center.x, y: center.y, width: width*3, height: height*3))
+//                let croppedImage = cropImage(center: center, width: width, height: height, inputImage: compressedImage!)
+                UIImageWriteToSavedPhotosAlbum(croppedImage!, nil, nil, nil)
+            }
+        }
+    }
+    
+    func cropImage(center: CGPoint, width: CGFloat, height: CGFloat, inputImage: UIImage)-> UIImage? {
+        let cropZone = CGRect(x: center.x, y: center.y, width: width, height: height)
+        
+        guard let cutImageRef: CGImage = inputImage.cgImage?.cropping(to: cropZone) else { return nil}
+        return UIImage(cgImage: cutImageRef, scale: inputImage.imageRendererFormat.scale, orientation: inputImage.imageOrientation)
+    }
+    
+}
