b8807a3ca0c359d46a18ea014859f778fd5b9351
Lets <ruvan123@naver.com>
2023-05-10 01:03:33 +0900

    feat: Make FaceTracking ViewControllerRepresentable and ViewController

---
+++ podong4cuts/podong4cuts/Src/Team2_AR/YoungIlDaeFaceTrackingViewController.swift
@@ -0,0 +1,109 @@
+//
+//  YoungIlDaeFaceTrackingViewController.swift
+//  podong4cuts
+//
+//  Created by user on 2023/05/10.
+//
+
+import Foundation
+
+import UIKit
+import RealityKit
+import ARKit
+
+class YoungIlDaeFaceTrackingViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+//    let model01 = try! Entity.load(named: "Paperbag_Mask")
+    let model01 = try! Entity.load(named: "Floating_Fox")
+//    let model02 = try! Experience.loadBox()
+//    let model02 = try! Entity.load(named: "Floating_Fox")
+    let model02 = try! Experience.loadBox()
+    let model03 = try! Entity.load(named: "Paperbag_Mask")
+    
+    lazy var models: [Any] = [self.model01, self.model02, self.model03]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+//        arView = ARView()
+        
+        arView = setupARView()
+        view.addSubview(arView)
+        NSLayoutConstraint.activate([
+            arView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            arView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            arView.topAnchor.constraint(equalTo: view.topAnchor),
+            arView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let configuration = ARFaceTrackingConfiguration()
+        configuration.maximumNumberOfTrackedFaces = 3
+        arView.contentScaleFactor = 0.75 * arView.contentScaleFactor
+        arView.session.run(configuration)
+    }
+    
+    func setupARView() -> ARView {
+        let frameRect = CGRect(x: 0, y: 0, width: 100, height: 100)
+        
+        let arView = ARView(frame: frameRect)
+        arView.session.delegate = self
+        arView.translatesAutoresizingMaskIntoConstraints = false
+        return arView
+    }
+    
+    func startFaceDetection() {
+        let configuration = ARFaceTrackingConfiguration()
+        configuration.maximumNumberOfTrackedFaces = 3
+        
+        arView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
+    }
+    
+    func takeSnapShot() {
+        print("Snapshot taken")
+        
+        DispatchQueue.global(qos: .userInitiated).async { [weak self] in
+            self?.arView.snapshot(saveToHDR: false) { (image) in
+                let compressedImage = UIImage(data: (image?.pngData())!)
+                
+                UIImageWriteToSavedPhotosAlbum(compressedImage!, nil, nil, nil)
+            }
+        }
+    }
+    
+}
+
+extension YoungIlDaeFaceTrackingViewController: ARSessionDelegate {
+    func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+        arView.scene.anchors.removeAll()
+        guard let faceAnchor = anchors.first as? ARFaceAnchor else { return }
+        
+            
+        for i in (0..<anchors.count) {
+            let faceAnchor = anchors[i] as? ARFaceAnchor
+            let anchor = AnchorEntity(anchor: faceAnchor!)
+            anchor.addChild(models[i] as! Entity)
+            arView.scene.anchors.append(anchor)
+        }
+        
+//        print(anchors.count)
+//        print((anchors as? [ARFaceAnchor])!)
+//        print(faceAnchor)
+//        let anchor1 = AnchorEntity(anchor: faceAnchor)
+//        let anchor2 = AnchorEntity(anchor: faceAnchor)
+//        print(anchors.count)
+//        print(anchors)
+//        anchor1.addChild(model01)
+//        anchor2.addChild(model02)
+//        arView.scene.anchors.append(anchor1)
+//        arView.scene.anchors.append(anchor2)
+        
+    }
+    
+    
+}
